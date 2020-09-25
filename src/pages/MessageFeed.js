34c9643cf0d06db1aa517dcd37d3ea7730840208
Kahlil Enmond <slope22@hotmail.com>
2020-09-25 15:44:11 -0400

    added Message Feed code

--- src/pages/MessageFeed.js
+++ src/pages/MessageFeed.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React from 'react'
+import Menu from '../components/menu/Menu';
+import MessageComponent from '../components/messageComponent/MessageComponent';
+import MessageList from '../components/messageList/MessageList';
+import {getMessages} from '../services/dataService'
 
 class MessageFeed extends React.Component {
+    constructor(props){
+        super(props)
 
+        this.state={
+            messages:[]
+
+        }
+    }
+
+    
+    componentDidMount(){
+        getMessages().then(data => {
+            if(data.statusCode === 200){
+                this.setState({
+                    messages: data.messages
+                })
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className='MessageFeed'>
+                <Menu/>
+                <MessageList messages={this.state.messages}/>
+                
+            
+            
+            </div>
+        );
+            
+    }
 }
-
 export default MessageFeed;
