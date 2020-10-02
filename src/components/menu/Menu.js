ff446f97318e40aac3b27b3facc788e0ada266b5
Lorne J <biglornej@gmail.com>
2020-10-02 09:20:45 -0600

    Updated profile page

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,20 +1,31 @@
 import React from "react";
 import "./Menu.css";
 import { withAsyncAction } from "../../redux/HOCs";
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import madDog from "../../images/angry-chihuahua.jpg";
 
 class Menu extends React.Component {
-  handleLogout = event => {
+  handleLogout = (event) => {
     event.preventDefault();
     this.props.logout();
   };
 
   render() {
     return (
-      <div className="Menu">
-        <Navbar bg="light" expand="lg">
-          <h1>Kwitter</h1>
+      // <div className="Menu">
+        <Navbar bg="light" expand="lg" sticky="top">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={madDog}
+              width="60"
+              height="60"
+              className="d-inline-block align-top"
+            />{" "}
+            {/* Misfitz Unite! */}
+          </Navbar.Brand>
+          <h1>Misfitz Unite!</h1>
           {this.props.isAuthenticated && (
             <div id="menu-links">
               <Nav className="mr-auto">
@@ -27,7 +38,7 @@ class Menu extends React.Component {
             </div>
           )}
         </Navbar>
-      </div>
+      // </div>
     );
   }
 }
--- src/components/userProfile/UserProfile.js
+++ src/components/userProfile/UserProfile.js
@@ -1,60 +1,62 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import UserUpdater from '../userUpdater/UserUpdater';
-import ghostUser from '../../assets/ghostuser.png';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import UserUpdater from "../userUpdater/UserUpdater";
+import ghostUser from "../../assets/ghostuser.png";
 
 class UserProfile extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            showModal: false
-        };
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  showModal = (e) => {
+    this.setState({ showModal: true });
+  };
+
+  hideModal = (e) => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    let button = null;
+    let url = this.props.user.pictureLocation
+      ? "https://socialapp-api.herokuapp.com"
+      : "";
+
+    if (this.props.username === this.props.match.params.username) {
+      button = (
+        <Button block variant="primary" onClick={this.showModal}>
+          Update Profile
+        </Button>
+      );
     }
 
-    showModal = e => {
-        this.setState({ showModal: true });
-    }
-
-    hideModal = e => {
-        this.setState({ showModal: false });
-    }
-
-    render() {
-        let button = null;
-        let url = this.props.user.pictureLocation ? 'https://socialapp-api.herokuapp.com' : '';
-
-        if(this.props.username === this.props.match.params.username) {
-            button = (
-                <Button block variant="primary" onClick={this.showModal}>
-                    Update Profile
-                </Button>
-            );
-        }
-        
-        return (
-            <div className="UserPicture">
-                <Card style={{ width: '25rem' }}>
-                    <Card.Img variant='top' src={url ? url + this.props.user.pictureLocation : ghostUser} />
-                    <Card.Body>
-                        <Card.Title>
-                            {this.props.user.displayName}
-                        </Card.Title>
-                        <Card.Text>
-                            {this.props.user.about}
-                        </Card.Text>
-                    </Card.Body>
-                    {button}
-                    <UserUpdater
-                        {...this.props}
-                        onClose={this.hideModal}
-                        show={this.state.showModal}
-                    />
-                </Card>
-            </div>
-        );
-    }
+    return (
+      <div className="UserPicture">
+        <Card style={{ width: "25rem" }} border = "white">
+          <Card.Img
+            class="rounded-circle"
+            variant="top"
+            src={url ? url + this.props.user.pictureLocation : ghostUser}
+          />
+          <Card.Body>
+            <Card.Title>{this.props.user.displayName}</Card.Title>
+            <Card.Text>{this.props.user.about}</Card.Text>
+          </Card.Body>
+          {button}
+          <UserUpdater
+            {...this.props}
+            onClose={this.hideModal}
+            show={this.state.showModal}
+          />
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default UserProfile;
